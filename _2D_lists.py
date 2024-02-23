2a29225aae875e0b7af382aa0699ef81bbf1107c
sergey <sl1ckypwnzip@gmail.com>
2024-02-23 14:08:45 +0300

    Добавьте файлы проекта.

---
+++ _2D_lists.py
@@ -0,0 +1,29 @@
+﻿import random
+m = int(input('m = '))
+n = int(input('n = '))
+
+matrix_1=[[random.randint(1, 11) for j in range(n)] for i in range(m) ]
+
+print('Matrix 1: ')
+
+for i in range(m):
+
+    print(matrix_1[i])
+
+matrix_2 = [ [ random.randint(1, 11) for j in range(n)] for i in range(m) ]
+
+print('Matrix 2: ')
+
+for i in range(m):
+
+    print(matrix_2[i])
+
+result = [[matrix_1[i][j] + matrix_2[i][j]  for j in range
+
+(len(matrix_1[0]))] for i in range(len(matrix_1))]
+
+print("Результат сложения двух матриц: ")
+
+for r in result:
+
+    print(r)
